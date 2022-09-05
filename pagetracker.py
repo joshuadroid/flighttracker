b1d4732a690c13d7c995eeec51942bc1c2e1a954
joshuadroid <joshuadroid@gmail.com>
2022-09-05 13:35:32 -0400

    changed flight tracker to pulling freefolk posts

--- pagetracker.py
+++ pagetracker.py
@@ -1,11 +1,16 @@
 ## GOAL: Gather the top 3 posts from r/freefolk and text them to me on Monday, Tuesday, and Wednesday
-
+## TODO filter out promoted
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 from datetime import datetime
 import os
 import sys
+
+app_path = os.path.dirname(sys.executable)
+
+now = datetime.now()
+month_day_year = now.strftime("%m%d%Y") #MMDDYYYY
 
 website = 'https://www.reddit.com/r/freefolk/top/'
 
@@ -25,5 +30,19 @@ driver.get(website)
 # Finds the top post and sends it. TODO make into a list and then loop to gather the top 3-5 posts.
 container = driver.find_element(by="xpath", value="//div[@data-testid='post-container']/div[3]/div[3]/div/div[2]/div/a/div/div/img").get_attribute("src")
 
+# Finds the date and time for logging the post
+date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+date, time = date_time.split(',')
+time = time.strip()
 
-# TODO filter out promoted
+# Takes data to log in csv format
+my_dict = {"Date": date, "Time": time, "PicLink": container.text}
+posts = pd.DataFrame(my_dict)
+
+# Setting up File path
+file_name = f'freefolkposts-{month_day_year}.csv'
+final_path = os.path.join(app_path, file_name)
+posts.to_csv(final_path)
+
+
+driver.quit()
